783131940d7523a37a9eb1dffe8ae17fd87f85c6
wasim tayyan <wasimtayyan@gmail.com>
2023-07-22 22:18:21 +0300

    finish

--- src/app/users/[id]/page.jsx
+++ src/app/users/[id]/page.jsx
@@ -1,18 +1,24 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import getUserPosts from "@/lib/getUserPost";
+
+
 function Page({ params }) {
+
   const [posts, setPost] = useState([]);
+  
   useEffect(() => {
+    console.log(params)
     getUserPosts(params.id).then((res) => setPost(res));
   }, []);
 
-  console.log(posts)
+
   const post = posts?.map((post) => {
     return(
-        <div key={post.id}>
-          <p>{post.id}</p>
-          <h1>{post.title}</h1>
+        <div key={post.id}
+        className="container mx-auto max-w-screen-lg px-4">
+          <p className="pt-2">`(${post.id})`</p>
+          <h1 className="font-bold py-2">{post.title}:</h1>
           <p>{post.body}</p>
         </div>)
       })
